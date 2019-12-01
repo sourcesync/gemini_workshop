5f46e0132fa4632019195cac7c06c4378a13dacc
George Williams <george.williams@gmail.com>
2019-11-30 23:54:24 -0800

    smarter helm upgrade script

--- setup/kubernetes/helm_upgrade.sh
+++ setup/kubernetes/helm_upgrade.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
+set -x
+set -x
+
 RELEASE=jhub
 
 helm upgrade $RELEASE jupyterhub/jupyterhub   --debug --version=0.8.2   --values config.yaml
 
-kubectl get service --namespace jhub
+./delete_all_pods.sh
 
+while True; do
+
+        kubectl get pods
+
+        kubectl get service --namespace jhub
+
+        sleep 1
+done
