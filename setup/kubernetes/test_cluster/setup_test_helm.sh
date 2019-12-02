6811109c642a5af8e9e52c4c256d9f2ea8d80bec
George Williams <george.williams@gmail.com>
2019-12-02 11:04:06 -0800

    starting the workshop notebook

---
+++ setup/kubernetes/test_cluster/setup_test_helm.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -x
+set -e
+
+curl https://raw.githubusercontent.com/kubernetes/helm/master/scripts/get | bash
+
+kubectl --namespace kube-system create serviceaccount tiller
+
+kubectl create clusterrolebinding tiller --clusterrole cluster-admin --serviceaccount=kube-system:tiller
+
+helm init --service-account tiller --wait
+
+kubectl patch deployment tiller-deploy --namespace=kube-system --type=json --patch='[{"op": "add", "path": "/spec/template/spec/containers/0/command", "value": ["/tiller", "--listen=localhost:44134"]}]'
+
+helm version
+
