3cc44a0f0e240e97897825f6944d44cfb39fa58b
George Williams <george.williams@gmail.com>
2020-01-17 12:03:39 -0800

    trying to add mac support

--- demo/TUNNELS.sh
+++ demo/TUNNELS.sh
@@ -5,6 +5,7 @@
 
 ETH=ppp0
 PASS=george
+MAC=1
 
 ##################################
 
@@ -17,11 +18,30 @@ ping 192.168.88.221 -c 1 -W 1
 RET="$?"
 if [ ! "$RET" -eq "0" ]; then
 	echo "192.168.88.221 does not exist. Adding IP to interface=$ETH..."
-	ifconfig "$ETH:0" 192.168.88.221
-	RET="$?"
-	if [ ! "$RET" -eq "0" ]; then
-		echo "Could not add IP to interface=$ETH"
-		exit
+	if [ "$MAC" -eq "1" ]; then
+		echo "creating 172 virtual IP for mac..."
+		ifconfig lo0 alias 172.17.0.1
+		RET="$?"
+		if [ ! "$RET" -eq "0" ]; then
+			echo "Could not add IP to interface=lo0"
+			exit
+		fi
+
+		echo "creating 192 virtual IP for mac..."
+		ifconfig lo0 alias 192.168.88.221
+		RET="$?"
+		if [ ! "$RET" -eq "0" ]; then
+			echo "Could not add IP to interface=lo0"
+			exit
+		fi
+	else
+		echo "creating virtual IP for linux..."
+		ifconfig "$ETH:0" 192.168.88.221
+		RET="$?"
+		if [ ! "$RET" -eq "0" ]; then
+			echo "Could not add IP to interface=$ETH"
+			exit
+		fi
 	fi
 else
 	echo "The IP is reachable."
@@ -38,19 +58,40 @@ ports=(4999 5000 7654 8093 8098 8097 8094 8091 6379 7707 8087 8085 8095 8099 543
 for i in "${ports[@]}"
 do
 	echo "creating tunnel for $i"
-	sshpass -p "$PASS" ssh -L 172.17.0.1:$i:172.17.0.1:$i george@192.168.99.21 -fN
-	RET=$?
-	if [ ! "$RET" -eq "0" ]; then
-		echo "Could not create the tunnel for port=$i"
-		exit
+	if [ "$MAC" -eq "1" ]; then
+		echo "creating for mac..."
+		ssh -L 172.17.0.1:$i:172.17.0.1:$i george@192.168.99.21 -fN
+		RET=$?
+		if [ ! "$RET" -eq "0" ]; then
+			echo "Could not create the tunnel for port=$i"
+			exit
+		fi
+	else
+		echo "creating for linux..."
+		sshpass -p "$PASS" ssh -L 172.17.0.1:$i:172.17.0.1:$i george@192.168.99.21 -fN
+		RET=$?
+		if [ ! "$RET" -eq "0" ]; then
+			echo "Could not create the tunnel for port=$i"
+			exit
+		fi
 	fi
 done
 
 echo "creating tunnel for 8090"
-sshpass -p "$PASS" ssh -L 192.168.88.221:8090:172.17.0.1:8090 george@192.168.99.21 -fN
-if [ ! "$RET" -eq "0" ]; then
-	echo "Could not create the tunnel for port=$i"
-	exit
+if [ "$MAC" -eq "1" ]; then
+	echo "creating for mac..."
+	ssh -L 192.168.88.221:8090:172.17.0.1:8090 george@192.168.99.21 -fN
+	if [ ! "$RET" -eq "0" ]; then
+		echo "Could not create the tunnel for port=$i"
+		exit
+	fi
+else
+	echo "creating for linux..."
+	sshpass -p "$PASS" ssh -L 192.168.88.221:8090:172.17.0.1:8090 george@192.168.99.21 -fN
+	if [ ! "$RET" -eq "0" ]; then
+		echo "Could not create the tunnel for port=$i"
+		exit
+	fi
 fi
 
 echo "Done."
